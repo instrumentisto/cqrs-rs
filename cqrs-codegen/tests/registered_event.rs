60902c6ef53a79f5e76ae73ba92b2b0c3fadcecf
tyranron <tyranron@gmail.com>
2019-11-05 14:43:23 +0200

    Improve tests with deeply nested case

--- cqrs-codegen/tests/registered_event.rs
+++ cqrs-codegen/tests/registered_event.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use std::any::TypeId;
 
 use cqrs::RegisteredEvent as _;
@@ -51,14 +53,48 @@ fn derives_for_enum() {
 
     assert_eq!(
         TestEvent::TestEventTuple(Default::default()).type_id(),
-        TypeId::of::<TestEvent1>()
+        TypeId::of::<TestEvent1>(),
     );
     assert_eq!(
         TestEvent::TestEventStruct {
             event: Default::default()
         }
         .type_id(),
-        TypeId::of::<TestEvent2>()
+        TypeId::of::<TestEvent2>(),
+    );
+}
+
+#[test]
+fn derives_for_deeply_nested_enum() {
+    #[derive(Default, Event, RegisteredEvent)]
+    #[event(type = "test.event.1")]
+    struct TestEvent1;
+
+    #[derive(Default, Event, RegisteredEvent)]
+    #[event(type = "test.event.2")]
+    struct TestEvent2;
+
+    #[derive(Event, RegisteredEvent)]
+    enum TestEvent {
+        TestEventTuple(TestEvent1),
+        TestEventStruct { event: TestEvent2 },
+    }
+
+    #[derive(Event, RegisteredEvent)]
+    enum TestEventNested {
+        TestEvent(TestEvent),
+    }
+
+    assert_eq!(
+        TestEventNested::TestEvent(TestEvent::TestEventTuple(Default::default())).type_id(),
+        TypeId::of::<TestEvent1>(),
+    );
+    assert_eq!(
+        TestEventNested::TestEvent(TestEvent::TestEventStruct {
+            event: Default::default()
+        })
+        .type_id(),
+        TypeId::of::<TestEvent2>(),
     );
 }
 
@@ -110,24 +146,24 @@ fn derives_for_generic_enum() {
 
     assert_eq!(
         TestEvent::TestEventTuple(Default::default()).type_id(),
-        TypeId::of::<TestEvent1>()
+        TypeId::of::<TestEvent1>(),
     );
     assert_eq!(
         TestEvent::TestEventStruct {
             event: Default::default()
         }
         .type_id(),
-        TypeId::of::<TestEvent2>()
+        TypeId::of::<TestEvent2>(),
     );
     assert_eq!(
         TestEvent::TestEventTupleGeneric(Default::default()).type_id(),
-        TypeId::of::<TestEventGeneric1<i32, String>>()
+        TypeId::of::<TestEventGeneric1<i32, String>>(),
     );
     assert_eq!(
         TestEvent::TestEventStructGeneric {
             event: Default::default()
         }
         .type_id(),
-        TypeId::of::<TestEventGeneric2<i32, String>>()
+        TypeId::of::<TestEventGeneric2<i32, String>>(),
     );
 }
