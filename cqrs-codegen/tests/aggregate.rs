d623ba8bfbfa9e9b12b1513165c51b9551d90074
50U10FCA7 <m.bashilov.srg@gmail.com>
2022-07-13 15:27:33 -0500

    Corrections

--- cqrs-codegen/tests/aggregate.rs
+++ cqrs-codegen/tests/aggregate.rs
@@ -115,7 +115,7 @@ fn derives_for_generic_struct_with_redundantly_explicit_id_field() {
 fn derives_for_generic_tuple_struct_with_explicit_id_field() {
     #[derive(Default, Aggregate)]
     #[aggregate(type = "test.aggregate")]
-    struct TestAggregate<T: Default>(#[aggregate(id)] T, T);
+    struct TestAggregate<T: Default = ()>(#[aggregate(id)] T, T);
 
     assert_eq!(TestAggregate::<i32>::AGGREGATE_TYPE, "test.aggregate");
     assert_eq!(
@@ -126,28 +126,10 @@ fn derives_for_generic_tuple_struct_with_explicit_id_field() {
 }
 
 #[test]
-fn derives_for_generic_struct_with_default_type() {
+fn derives_for_struct_with_const_generic_parameters() {
     #[derive(Default, Aggregate)]
     #[aggregate(type = "test.aggregate")]
-    struct TestAggregate<T: Default = i32> {
-        #[aggregate(id)]
-        id: i32,
-
-        field: T,
-    }
-
-    type TestAgg = TestAggregate;
-
-    assert_eq!(TestAgg::AGGREGATE_TYPE, "test.aggregate");
-    assert_eq!(TestAgg::default().aggregate_type(), "test.aggregate");
-    assert_eq!(*TestAgg::default().id(), 0);
-}
-
-#[test]
-fn derives_for_const_generic_struct() {
-    #[derive(Default, Aggregate)]
-    #[aggregate(type = "test.aggregate")]
-    struct TestAggregate<const T: u8>(#[aggregate(id)] i32);
+    struct TestAggregate<const T: u8 = 0>(#[aggregate(id)] i32);
 
     assert_eq!(TestAggregate::<1>::AGGREGATE_TYPE, "test.aggregate");
     assert_eq!(
@@ -155,17 +137,4 @@ fn derives_for_const_generic_struct() {
         "test.aggregate"
     );
     assert_eq!(*TestAggregate::<1>::default().id(), 0);
-}
-
-#[test]
-fn derives_for_const_generic_struct_with_default_value() {
-    #[derive(Default, Aggregate)]
-    #[aggregate(type = "test.aggregate")]
-    struct TestAggregate<const T: u8 = 0>(#[aggregate(id)] i32);
-
-    type TestAgg = TestAggregate;
-
-    assert_eq!(TestAgg::AGGREGATE_TYPE, "test.aggregate");
-    assert_eq!(TestAgg::default().aggregate_type(), "test.aggregate");
-    assert_eq!(*TestAgg::default().id(), 0);
 }
